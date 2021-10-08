ece24314f457f56f91ed3cf48a7c43558ddd900b
Jean-César <jeancesar.bazin@gmail.com>
2021-10-08 13:41:06 +0200

    implementing tests

--- src/test/java/Mediscreenmspatient/MediscreenMsPatientApplicationTests.java
+++ src/test/java/Mediscreenmspatient/MediscreenMsPatientApplicationTests.java
@@ -2,12 +2,18 @@ package Mediscreenmspatient;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootTest
 class MediscreenMsPatientApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+	@Test
+	public void createPatientTest(){
+
+
 	}
 
 }
